d753b94c24f848abd0a0d8ffcb2e01a98ce3dc12
BALA RAVI <bala444learning@gmail.com>
2026-05-26 17:32:34 +0530

    10_ai_student_profile.py

---
+++ days/day-01/code/10_ai_student_profile.py
@@ -0,0 +1,11 @@
+# Problem 10 - AI Student Profile Program
+# Day 01 - Python Fundamentals
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+language = input("Enter favorite language: ")
+print("\n--- AI Student Profile ---")
+print("Name:", name)
+print("Age:", age)
+print("Language:", language)
+print("--------------------------")
