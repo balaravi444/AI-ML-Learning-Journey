962753674028448c5ce5645b59efa321e89a9797
BALA RAVI <bala444learning@gmail.com>
2026-05-27 21:50:12 +0530

    positive_negative_zero.py

---
+++ days/day-03/code/02/_positive_negative_zero.py
@@ -0,0 +1,11 @@
+# Problem 2 -Positive, Negative or Zero
+# Day 03 _Control Flow
+
+num = int(input("Enter a number: "))
+if num > 0:
+  print("Positive")
+elif num < 0:
+  print("Negative")
+else:
+  print("Zero")
+  
