e46030131c02d8f5b01742ee32d1d2bf8ab5a2bc
BALA RAVI <bala444learning@gmail.com>
2026-05-28 19:02:06 +0530

    04_sum_and_largest.py

---
+++ days/day-07/code/04_sum_and_largest.py
@@ -0,0 +1,6 @@
+# Problem 4 - Finding Sum and Largest Element
+# Day 07 - Sets, Arrays & List Comprehensions
+
+numbers = [10, 45, 22, 78, 15]
+print("Sum:", sum(numbers))
+print("Largest:", max(numbers))
