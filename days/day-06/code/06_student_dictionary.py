575d27db82e8c80d5b9f20c18a3b9e3a133806fd
BALA RAVI <bala444learning@gmail.com>
2026-05-28 18:54:34 +0530

    06_student_dictionary.py

---
+++ days/day-06/code/06_student_dictionary.py
@@ -0,0 +1,14 @@
+# Problem 6 - Student Dictionary Program
+# Day 06 - Python Data Structures
+
+student = {
+    "name": "Bala",
+    "age": 20,
+    "course": "BCA",
+    "goal": "AI/ML Engineer"
+}
+print("Student Profile")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
+print("Goal:", student["goal"])
