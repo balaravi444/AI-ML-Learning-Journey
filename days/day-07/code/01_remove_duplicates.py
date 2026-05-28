80c99b374e263e3c9782a3b87bde857d3f47bf18
BALA RAVI <bala444learning@gmail.com>
2026-05-28 19:00:39 +0530

    01_remove_duplicates.py

---
+++ days/day-07/code/01_remove_duplicates.py
@@ -0,0 +1,6 @@
+# Problem 1 - Removing Duplicates Using Sets
+# Day 07 - Sets, Arrays & List Comprehensions
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+unique = set(numbers)
+print("Unique numbers:", unique)
