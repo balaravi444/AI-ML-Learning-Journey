0a72a3688fb546da06ba9eda3113b10bcb09480c
BALA RAVI <bala444learning@gmail.com>
2026-05-28 18:50:46 +0530

    05_recursive_sum.py

---
+++ days/day-05/code/05_recursive_sum.py
@@ -0,0 +1,9 @@
+# Problem 5 - Recursive Sum of First N Numbers
+# Day 05 - Advanced Functions & Recursion
+
+def sum_n(n):
+    if n == 0:
+        return 0
+    return n + sum_n(n - 1)
+
+print("Sum =", sum_n(5))
