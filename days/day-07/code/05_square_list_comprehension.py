fb931dae2dedefe39deaff9f7cab04345a6086b0
BALA RAVI <bala444learning@gmail.com>
2026-05-28 19:02:33 +0530

    05_square_list_comprehension.py

---
+++ days/day-07/code/05_square_list_comprehension.py
@@ -0,0 +1,6 @@
+# Problem 5 - Creating Square Lists Using List Comprehensions
+# Day 07 - Sets, Arrays & List Comprehensions
+
+numbers = [1, 2, 3, 4, 5]
+squares = [n * n for n in numbers]
+print("Squares:", squares)
